6234b9d272fc656d2ba7aab111959b6e318deffe
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-07-19 19:35:11 -0300

    Dashboard - inicio

---
+++ thdash/src/App.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar/Sidebar";
+
+export default function App() {  
+
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+ 
---
+++ thdash/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,65 @@
+import './Sidebar.css'
+import logo from '../../assets/sliders.png'
+
+export default function Sidebar() {
+  return (
+    <div className="container">
+      <aside>
+        <div className="top">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <h2>
+              EGA<span className="danger">TOR</span>
+            </h2>
+          </div>
+          <div className="close" id="close-btn">
+            <span className="material-symbols-sharp">close</span>
+          </div>
+        </div>
+        <div className="sidebar">
+          <a href="#">
+            <span className="material-symbols-sharp">dashboard</span>
+            <h3>Dashboard</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">person_outline</span>
+            <h3>Customers</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">receipt_long</span>
+            <h3>Orders</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">insights</span>
+            <h3>Analytics</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">mail_otline</span>
+            <h3>Messages</h3>
+            <span className="message-count">26</span>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">inventory</span>
+            <h3>Products</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">report_gmailerrorred</span>
+            <h3>Reports</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">settings</span>
+            <h3>Settings</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">add</span>
+            <h3>Add Product</h3>
+          </a>
+          <a href="#">
+            <span className="material-symbols-sharp">logout</span>
+            <h3>Logout</h3>
+          </a>
+        </div>
+      </aside>
+    </div>
+  )
+}
